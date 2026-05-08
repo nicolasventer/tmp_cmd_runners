50a9baa6abb52516703d3eee840c7a2665a2bdad
Nicolas VENTER <thedeal78@hotmail.fr>
2026-05-08 16:31:59 +0200

    bundle ts

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 bun build --compile --target=browser ./client/index.html --outdir=ts_server
-bun build --compile ./ts_server/src/index.ts --outfile ./dist/command-runners
+bun build --compile ./server/src/index.ts --outfile ./dist/command-runners
